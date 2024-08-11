1407ecbfddfe739e46e4e30806ab619c2194650e
John Wilson <mail@johnwilson.me.uk>
2024-08-11 21:05:21 +0100

    r - inline

--- src/test/java/train/the/trainer/FizzBuzzShould.java
+++ src/test/java/train/the/trainer/FizzBuzzShould.java
@@ -9,56 +9,48 @@ class FizzBuzzShould {
     @Test
     void return_1_given_1() {
         FizzBuzz fizzBuzz = new FizzBuzz();
-        String actual = fizzBuzz.convert(1);
-        assertEquals("1", actual);
+        assertEquals("1", fizzBuzz.convert(1));
     }
 
     @Test
     void return_2_given_2() {
         FizzBuzz fizzBuzz = new FizzBuzz();
-        String actual = fizzBuzz.convert(2);
-        assertEquals("2", actual);
+        assertEquals("2", fizzBuzz.convert(2));
     }
 
     @Test
     void return_fizz_given_3() {
         FizzBuzz fizzBuzz = new FizzBuzz();
-        String actual = fizzBuzz.convert(3);
-        assertEquals("Fizz", actual);
+        assertEquals("Fizz", fizzBuzz.convert(3));
     }
 
     @Test
     void return_fizz_given_6() {
         FizzBuzz fizzBuzz = new FizzBuzz();
-        String actual = fizzBuzz.convert(6);
-        assertEquals("Fizz", actual);
+        assertEquals("Fizz", fizzBuzz.convert(6));
     }
 
     @Test
     void return_buzz_given_5() {
         FizzBuzz fizzBuzz = new FizzBuzz();
-        String actual = fizzBuzz.convert(5);
-        assertEquals("Buzz", actual);
+        assertEquals("Buzz", fizzBuzz.convert(5));
     }
 
     @Test
     void return_buzz_given_10() {
         FizzBuzz fizzBuzz = new FizzBuzz();
-        String actual = fizzBuzz.convert(10);
-        assertEquals("Buzz", actual);
+        assertEquals("Buzz", fizzBuzz.convert(10));
     }
 
     @Test
     void return_fizzbuzz_given_15() {
         FizzBuzz fizzBuzz = new FizzBuzz();
-        String actual = fizzBuzz.convert(15);
         assertEquals("FizzBuzz", fizzBuzz.convert(15));
     }
 
     @Test
     void return_fizzbuzz_given_30() {
         FizzBuzz fizzBuzz = new FizzBuzz();
-        String actual = fizzBuzz.convert(30);
         assertEquals("FizzBuzz", fizzBuzz.convert(30));
     }
 
