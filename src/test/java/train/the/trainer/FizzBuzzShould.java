770f4ee150dfcabd0c0212024822b93043d90d8a
John Wilson <mail@johnwilson.me.uk>
2024-08-11 20:07:06 +0100

    r - add test

---
+++ src/test/java/train/the/trainer/FizzBuzzShould.java
@@ -0,0 +1,40 @@
+package train.the.trainer;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FizzBuzzShould {
+
+    @Test
+    void return_1_given_1() {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        assertEquals("1", fizzBuzz.convert(1));
+    }
+
+    @Test
+    void return_2_given_2() {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        assertEquals("2", fizzBuzz.convert(2));
+    }
+
+    @Test
+    void return_fizz_given_3() {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        assertEquals("Fizz", fizzBuzz.convert(3));
+    }
+
+    @Test
+    void return_buzz_given_5() {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        assertEquals("Buzz", fizzBuzz.convert(5));
+    }
+
+    @Test
+    void return_fizzbuzz_given_15() {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        assertEquals("Buzz", fizzBuzz.convert(5));
+    }
+
+
+}
