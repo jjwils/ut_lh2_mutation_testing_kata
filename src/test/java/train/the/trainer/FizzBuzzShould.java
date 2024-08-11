4cc5b6c62bb7af3a89b6076479e89ade205eefbc
John Wilson <mail@johnwilson.me.uk>
2024-08-11 20:51:38 +0100

    R** - making the deliberate mistake more believable

--- src/test/java/train/the/trainer/FizzBuzzShould.java
+++ src/test/java/train/the/trainer/FizzBuzzShould.java
@@ -4,38 +4,38 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class FizzBuzzTest {
+class FizzBuzzShould {
 
     @Test
     void return_1_given_1() {
         FizzBuzz fizzBuzz = new FizzBuzz();
         String actual = fizzBuzz.convert(1);
-        assertEquals("1", actual);
+        assertEquals("1", fizzBuzz.convert(1));
     }
 
     @Test
     void return_2_given_2() {
         FizzBuzz fizzBuzz = new FizzBuzz();
         String actual = fizzBuzz.convert(2);
-        assertEquals("2", actual);
+        assertEquals("2", fizzBuzz.convert(2));
     }
 
     @Test
     void return_fizz_given_3() {
         FizzBuzz fizzBuzz = new FizzBuzz();
         String actual = fizzBuzz.convert(3);
-        assertEquals("Fizz", actual);
+        assertEquals("Fizz", fizzBuzz.convert(3));
     }
 
     @Test
     void return_buzz_given_5() {
         FizzBuzz fizzBuzz = new FizzBuzz();
         String actual = fizzBuzz.convert(5);
-        assertEquals("Buzz", actual);
+        assertEquals("Buzz", fizzBuzz.convert(5));
     }
 
     @Test
-    void return_fizzbuzz_given_15() {
+    void return_fizz_buzz_given_15() {
         FizzBuzz fizzBuzz = new FizzBuzz();
         String actual = fizzBuzz.convert(15);
         assertEquals("Buzz", fizzBuzz.convert(5));
